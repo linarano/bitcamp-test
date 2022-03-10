0b3260300de101d3bc39cf5fe20f74f8dd91f034
wkdckd41 <wkdckd41@gmail.com>
2022-03-10 17:46:53 +0900

    ..

--- app/src/main/java/com/bitcamptest/Hello1.java
+++ app/src/main/java/com/bitcamptest/Hello1.java
@@ -5,6 +5,10 @@ public class Hello1 {
   public static void main(String[] args) {
     System.out.println("장창!");
 
-  }
+    m9999();
 
+  }
+  static void m9999() {
+    System.out.println("오호라");
+  }
 }
